3788924d301d7b5205c5d04fc75f7ab87486e494
Lena8829 <illenarionova@gmail.com>
2024-03-10 12:58:00 +0200

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,15 +1,32 @@
-function filterArray(numbers, value) {
-  let array = [];
+// Об’єкт profile описує профіль користувача на ігровій платформі. У його властивостях зберігається ім’я профілю username та кількість активних годин playTime, проведених у грі.
 
-  for (const num of numbers) {
-    if (num > value) {
-      array.push(num);
-    }
-  }
-  return array;
-}
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+// Доповни об’єкт profile методами для роботи з його властивостями.
+
+// Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName та змінювати значення властивості username на нове. Нічого не повертає.
+// Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours та збільшити на нього значення властивості playTime. Нічого не повертає.
+// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!, де <Username> — це ім’я профілю, а <amount> — кількість ігрових годин.
+
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+
+  updatePlayTime(hours) {
+    return (this.playTime += hours);
+  },
+
+  changeUsername(newName) {
+    return (this.username = newName);
+  },
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
